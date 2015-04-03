6ad05d534a5a045231e61059afb04a2eb14cefdb
Artem Gorokhov <brannt@yandex.ru>
2015-04-03 15:30:47 +0300

    Add necessary info for PyPi release

--- setup.py
+++ setup.py
@@ -12,10 +12,19 @@ if __name__ == '__main__':
         name='noseapp_requests',
         url='https://github.com/brannt/noseapp_requests',
         version=__version__,
-        packages=find_packages(),
+        packages=find_packages(exclude=['docs', 'test*']),
         author='Artem Gorokhov',
         author_email='a.gorohov@corp.mail.ru',
         description='requests extension for noseapp lib',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Testing',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+        ],
+        keywords='noseapp requests testing',
         include_package_data=True,
         zip_safe=False,
         platforms='any',
@@ -23,4 +32,7 @@ if __name__ == '__main__':
             'noseapp',
             'requests==2.6.0',
         ],
+        extras_require={
+            'doc': ['Sphinx']
+        }
     )
